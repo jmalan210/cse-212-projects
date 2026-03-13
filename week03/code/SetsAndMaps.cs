9fc59fdd1e1f629181119bbd27d4a59af95ac880
jmalan210 <>
2026-03-13 11:32:54 -0600

    W03 Code: Problem 2 passed related test

--- week03/code/SetsAndMaps.cs
+++ week03/code/SetsAndMaps.cs
@@ -57,7 +57,18 @@ public static class SetsAndMaps
         foreach (var line in File.ReadLines(filename))
         {
             var fields = line.Split(",");
-            // TODO Problem 2 - ADD YOUR CODE HERE
+            string degree = fields[3];
+
+            if (degrees.ContainsKey(degree))
+            {
+                degrees[degree]++;
+            }
+            
+            else
+            {
+                degrees[degree] = 1;
+            }
+
         }
 
         return degrees;
