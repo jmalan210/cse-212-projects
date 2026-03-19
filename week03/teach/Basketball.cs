f88b316fad3baa8326e2ca2f2c67a793ba624330
jmalan210 <>
2026-03-19 14:15:34 -0600

    W03 Team Assignment

--- week03/teach/Basketball.cs
+++ week03/teach/Basketball.cs
@@ -23,14 +23,33 @@ public class Basketball
         reader.TextFieldType = FieldType.Delimited;
         reader.SetDelimiters(",");
         reader.ReadFields(); // ignore header row
-        while (!reader.EndOfData) {
+        while (!reader.EndOfData)
+        {
             var fields = reader.ReadFields()!;
             var playerId = fields[0];
             var points = int.Parse(fields[8]);
+
+            if (!players.ContainsKey(playerId))
+            {
+                players.Add(playerId, points);
+            }
+
+            else if (players.ContainsKey(playerId))
+            {
+                players[playerId] += points;
+            }
         }
+        
+       
 
-        Console.WriteLine($"Players: {{{string.Join(", ", players)}}}");
-
-        var topPlayers = new string[10];
+        // Console.WriteLine($"Players: {{{string.Join(", ", players)}}}");
+        
+        var topPlayers = players.ToArray();
+        Array.Sort(topPlayers, (p1, p2) => p2.Value - p1.Value);
+        Console.WriteLine();
+        for (var i=0; i<10; i++)
+        {
+            Console.WriteLine(topPlayers[i]);
+        }
     }
 }
--- week03/teach/Program.cs
+++ week03/teach/Program.cs
@@ -1,8 +1,8 @@
 ﻿Console.WriteLine("\n======================\nUnique Letters\n======================");
-UniqueLetters.Run();
+// UniqueLetters.Run();
 
-Console.WriteLine("\n======================\nDisplay Sums\n======================");
-DisplaySums.Run();
+// Console.WriteLine("\n======================\nDisplay Sums\n======================");
+// DisplaySums.Run();
 
 Console.WriteLine("\n======================\nBasketball\n======================");
 Basketball.Run();
@@ -11,8 +11,8 @@ Basketball.Run();
 // Console.WriteLine("\n======================\nUnique Letters\n======================");
 // UniqueLettersSolution.Run();
 
-// Console.WriteLine("\n======================\nDisplay Sums\n======================");
+// Console.WriteLine("\n======================\nDisplay Sums Solution\n======================");
 // DisplaySumsSolution.Run();
 
-// Console.WriteLine("\n======================\nBasketball\n======================");
-// BasketballSolution.Run();
+Console.WriteLine("\n======================\nBasketball\n======================");
+BasketballSolution.Run();
